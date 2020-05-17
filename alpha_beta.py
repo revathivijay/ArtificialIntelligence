129a0986fb87b26e57b61d76af8b77a91b198d0f
revathivijay <34963337+revathivijay@users.noreply.github.com>
2020-05-17 16:58:49 +0530

    Update alpha_beta.py

--- alpha_beta.py
+++ alpha_beta.py
@@ -1,15 +1,10 @@
 import numpy as np
 
-# Initial values of Aplha and Beta 
 MAX, MIN = 10000, -10000
 
-# Returns optimal value for current player 
-#(Initially called for root and maximizer) 
 def minimax(depth, nodeIndex, maximizingPlayer, 
             values, alpha, beta, height): 
 
-    # Terminating condition. i.e 
-    # leaf node is reached 
     if depth == 3: 
         return values[nodeIndex] 
 
@@ -29,8 +24,8 @@ def minimax(depth, nodeIndex, maximizingPlayer,
             print("Changing alpha to: ", alpha)
             print("Beta: ", beta)
             print()
+            
             # Alpha Beta Pruning
-
             if beta <= alpha:
                 print("ALPHA CUTOFF")
                 break
@@ -39,9 +34,6 @@ def minimax(depth, nodeIndex, maximizingPlayer,
     
     else: 
         best = MAX
-
-        # Recur for left and 
-        # right children 
         for i in range(0, 2): 
         
             val = minimax(depth + 1, nodeIndex * 2 + i, 
@@ -60,8 +52,7 @@ def minimax(depth, nodeIndex, maximizingPlayer,
                 break
         
         return best 
-    
-# Driver Code 
+
 if __name__ == "__main__": 
 
     values = [3, 5, 6, 9, 1, 2, 0, -1]
@@ -69,5 +60,4 @@ if __name__ == "__main__":
     print("Leaf Nodes: ", values)
     print("Height of tree: ", height)
     print("The optimal value is :", minimax(0, 0, True, values, MIN, MAX, height)) 
-    
-# This code is contributed by Rituraj Jain 
+
